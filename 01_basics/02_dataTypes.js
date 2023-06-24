a1bfb60eb54095870e3081f498a24ae780640cff
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-24 13:20:23 +0000

    dataTypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code Newer version
+let a=7
+
+
+//number = 2
+//boolean = true/false
+//string = "gagan"
+//null = stand alone value
+//undefined = value is not defined
+//symbol = uniqueness
+
+//object
+console.log(typeof null)
+console.log(typeof undefined)
