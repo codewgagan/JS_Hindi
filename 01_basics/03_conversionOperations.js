fb0f2e9fe41994386c1e530e0543ab6604bda181
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-24 13:50:21 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,10 @@
+let score = "333abd"
+console.log(typeof score)
+
+let valueInNumber = Number (score);
+console.log(valueInNumber)
+
+let boolean = 1
+let booleanValue = Boolean (boolean)
+console.log(typeof boolean)
+console.log(booleanValue)
