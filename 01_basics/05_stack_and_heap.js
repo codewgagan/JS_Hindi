2d4aa41ab0f1971380ba0310d339b3378917dde8
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-24 14:57:53 +0000

    Stack and Heap is done

---
+++ 01_basics/05_stack_and_heap.js
@@ -0,0 +1,23 @@
+let myYtname = "code with Gagan";
+let anotherName = myYtname;
+
+anotherName = "code smashers"
+
+console.log(myYtname);
+console.log(anotherName);
+
+let gagan = {
+    name:"gagan",
+    id:1
+}
+
+let ramesh = {
+    name:"ramesh",
+    id:2
+}
+
+let userThree = ramesh
+userThree.name = "gauri"
+// console.log(gagan)
+console.log(gagan.id)
+console.log(userThree.name);
