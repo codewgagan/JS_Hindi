67d6c674d4a09f110a6adcea4b1554e62eb875b6
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-24 13:20:05 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 100213
+let accountEmail = "gagan@gmail.com"
+var accountPassword = "123456" 
+
+accountCity = "Banglore"
+accountEmail = "ramesh@gamil.com"
+accountPassword = "787324"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountCity, accountPassword])
