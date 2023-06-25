9ff35ca154ed9e756bb532ef3853df9f3a7bb613
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-25 05:25:39 +0000

    MAth and Number

---
+++ 01_basics/07_math_and_number.js
@@ -0,0 +1,22 @@
+//Numbers
+const score = 283.78657
+// console.log(score.toPrecision(5));
+// console.log(score.toFixed(3))
+
+const hundreds = 100000000.000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//Maths
+// console.log(Math.abs(hundreds));
+// console.log(Math.ceil(12.24));
+// console.log((Math.floor(32.99)));
+// console.log(Math.abs(-22.99));
+// console.log(Math.sqrt(4))
+// console.log(Math.pow(4,3))
+// console.log(Math.random())
+
+// console.log(Math.floor(Math.random()*6)+1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
