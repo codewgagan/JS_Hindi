10390c87fb953f5d4bdba3c811bcf3f4a34cec85
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-25 14:00:53 +0000

    Object part 2

---
+++ 02_basics/03_objects_part_2.js
@@ -0,0 +1,37 @@
+// const YtUser = new Object() //Declaring singleton object
+
+
+const YtUser = {} //Declaring non-singleton object
+YtUser.id = "0333123"
+YtUser.name = "Gagan"
+YtUser.isLoggedIn = false
+
+
+//Objects within object
+const user1 = {
+    email:"admin@gmail.com",
+    fullname: {
+        name:"Gagan",
+        sirName:"Gowda"
+    },
+    userProfile:{
+        username:"skyg",
+        password:"123321"
+    }
+}
+console.log(YtUser)
+console.log(user1)
+console.log(user1.userProfile.password)
+
+
+//combining objects like arrays
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+
+// const obj3 = {obj1,obj2}
+// const obj3 = Object.assign({}, obj1,obj2);  //The Object.assign() static method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object.
+// console.log(obj3)
+const obj3 = {...obj1,...obj2}  //spread
+console.log(obj3)
+// console.log(obj3===obj1);
+
