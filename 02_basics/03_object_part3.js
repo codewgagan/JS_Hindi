c99bbed615fb54ced662863d7314be59572c838f
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-25 14:16:09 +0000

    Object part 3

---
+++ 02_basics/03_object_part3.js
@@ -0,0 +1,14 @@
+//Destructuring the object to get values
+
+const course = {
+    courseanme:"Web dev",
+    price:"499/-",
+    courseInstructor:"Gagan"
+}
+console.log(course)
+const {price: cost} = course//changing the name of a price into cost and printing price 
+console.log(cost)
+
+const navbar = () => {
+    
+}
