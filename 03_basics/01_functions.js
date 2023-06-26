f8f13ae9bf2eda20197b960fac416359e02c4e10
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-26 17:51:10 +0000

    Functions Part 1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,31 @@
+//Simple printing name
+function sayMyName(){
+    console.log("Gagan")
+}
+sayMyName();//Reference is sayMyName and () execution part. 
+// sayMyName();
+
+
+
+//Adding two numbers
+function addTwoNumbers(num1,num2){
+    console.log(num1+num2);
+}
+addTwoNumbers(3,6)//inside () is arguments.
+
+//return
+function subTwoNum(num1,num2) {
+    let result = num1 - num2;
+    return result;
+}
+console.log("Result is" ,9-3);// return result
+
+function isUserLoggedIn(username){
+    if(!username){
+        console.log("Please enter your username")
+        return
+    }
+    return(username)
+
+}
+console.log(isUserLoggedIn())
