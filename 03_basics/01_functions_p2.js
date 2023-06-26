5849e69f49e6f0fd273e95bf4e80c3662c3797f9
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-26 19:25:51 +0000

    Functions part 2

---
+++ 03_basics/01_functions_p2.js
@@ -0,0 +1,17 @@
+//Cart in E-commerce
+//... thriple dot is REST
+
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(199,299,399))//stores the value in Array
+
+const user = {
+    username: "Gagan",
+    pwd: "23322332"
+}
+
+function handleUser(anyobject){
+    console.log(`username is ${anyobject.username} and pwd is ${anyobject.pwd}`)
+}
+handleUser(user)
