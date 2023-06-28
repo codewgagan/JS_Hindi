a07f69951ab3399588f1c4a45d5b74701fadb8dd
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-28 19:22:04 +0000

    Scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,18 @@
+
+
+//{} curley braces is a scope 
+//Dont get confused with object and scope.
+
+
+if(true){
+    let a = 11;
+    const b = 22
+    // var c = 33;
+}
+//why we dont use var in scope.The reason is if someone declares a same value in global scope and local scope then the value of global scope will not get printed in console
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
