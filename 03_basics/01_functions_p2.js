f31de3a4236cd7aba14786cd45f464a459a4d459
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-06-28 18:52:02 +0000

    Function part 2

--- 03_basics/01_functions_p2.js
+++ 03_basics/01_functions_p2.js
@@ -1,17 +1,24 @@
 //Cart in E-commerce
-//... thriple dot is REST
+//... triple dot is REST
 
 function calculateCartPrice(...num1){
     return num1
 }
-console.log(calculateCartPrice(199,299,399))//stores the value in Array
+console.log(calculateCartPrice(199,299,399,499,599))//stores the value in Array
 
 const user = {
     username: "Gagan",
-    pwd: "23322332"
+    pwd: "499"
 }
 
 function handleUser(anyobject){
-    console.log(`username is ${anyobject.username} and pwd is ${anyobject.pwd}`)
+    console.log(`username is ${anyobject.username} and price is ${anyobject.pwd}/-`)
 }
 handleUser(user)
+
+const myNewArray = [100,200,300]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+console.log(returnSecondValue(myNewArray))
