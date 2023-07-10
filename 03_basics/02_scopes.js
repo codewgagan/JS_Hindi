192b7be4fc9069f7bd63f3330f71691422c52ce2
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-10 13:40:53 +0000

    Scopes and Hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -13,6 +13,41 @@ if(true){
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
 
 
+// ############################## Scope and Hoisting ##############################
+
+function one(){
+    const username = "Gagan"
+
+    function two(){
+        const website = "Youtube"
+        // console.log(username);
+    }
+    // console.log(website)
+    two()
+    
+}
+one()
+
+if (true){
+    const username = "gagan"
+    if(username === "gagan"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website)
+    // console.log(username)
+}
+
+// ############################## Interesting ##############################
+console.log(addOne(5)) //Can access 'addOne' before initialization
+function addOne(num){
+    return num + 1
+}
+
+console.log(addtwo(5)) //Cannot access 'addtwo' before initialization
+const addtwo = function(num){
+    return num + 2
+}
