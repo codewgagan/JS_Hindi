bbb59d0a75d0037230a9b5e2b92b73505c688b30
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-13 00:08:18 +0000

    Call Stack

---
+++ 03_basics/05_call_stack.js
@@ -0,0 +1,11 @@
+//CALLL STACK
+//{}Execution Context
+//-> Global Execution Context
+//-> Function Execution Context
+//-> Eval Execution Context
+
+//->Memory Phase
+//->Execution Phase(It repeats) -> New variable environment and execution thread.
+
+let value2 = 10
+let value1 = 5
