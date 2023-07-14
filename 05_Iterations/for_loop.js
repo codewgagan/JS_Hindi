b4fa527585ba8ba196c25c5a409177843e422b73
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-14 12:50:42 +0000

    For loop

---
+++ 05_Iterations/for_loop.js
@@ -0,0 +1,24 @@
+for(let i = 0; i<10; i++){
+    const result = (`The range is ${i}`)
+    // console.log(result)
+}
+
+for(let i = 1; i<11; i++){
+    // const result =(`The First i loop ${i}`)
+    // console.log(result)
+    for(j=1; j<11; j++){
+        // const result2 = (`The Second j loop ${j}`)
+        // console.log(i + '*' + j + '=' + i*j); //Multiplication of table
+    }
+}
+
+//break and continue
+
+for(let i = 1; i<10; i++){
+    // console.log(i)
+    if (i===5) {
+        console.log(`5 is detected`)
+        break;
+    }
+    console.log('The value is' + i)
+}
