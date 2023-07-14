9c2cf9c70cd6718aabf1f470efaba64be943b916
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-14 13:05:02 +0000

    do while loop

---
+++ 05_Iterations/while_loop.js
@@ -0,0 +1,13 @@
+//while loop
+i=0
+while (i<=6) {
+    console.log(i);
+    i=i+2;
+}
+
+//do while loop
+let score = 0;
+do {
+    console.log('The value is ' + score)
+    score++;
+} while (score<=5);
