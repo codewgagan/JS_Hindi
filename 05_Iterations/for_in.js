f078f913e7926a625f1a92cb46a4a068c062641b
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-16 14:45:28 +0000

    For in loop

---
+++ 05_Iterations/for_in.js
@@ -0,0 +1,12 @@
+//for in loop
+const myObj = {
+    lang1: 'Html',
+    lang2: 'css',
+    lang3: 'javascript',
+    lang4: 'react'
+}
+for (const key in myObj) {
+        console.log(`${key} is ${myObj[key]}`)
+}
+
+
