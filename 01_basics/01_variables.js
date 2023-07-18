f4456e4400d4a8918f096c574f025179590a00c6
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-18 14:20:14 +0000

    Variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,5 +1,6 @@
-const accountId = 100213
-let accountEmail = "gagan@gmail.com"
+const accountId = 100213 //The const declaration declares block-scoped local variables. The value of a constant can't be changed through reassignment using the assignment operator, but if a constant is an object, its properties can be added, updated, or removed.
+let accountEmail = "gagan@gmail.com" //The let declaration declares re-assignable, block-scoped local variables, optionally initializing each to a value.
+
 var accountPassword = "123456" 
 
 accountCity = "Banglore"
