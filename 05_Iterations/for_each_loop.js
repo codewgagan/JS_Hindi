20348b4560c5ecb4b6c272a673ffccf69708a37c
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-19 14:33:28 +0000

    For each Loop

---
+++ 05_Iterations/for_each_loop.js
@@ -0,0 +1,15 @@
+// const coding = ["js", "html", "css", "cpp"]
+// coding.forEach( function (item) {
+//     // console.log(item)
+// } )
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+// } )
+// console.log(values)
+// for each use karte vakt call back function ka naam nahi hota hai.
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNums=  myNums.filter( (num) => num > 2 )
+console.log(newNums);
+
