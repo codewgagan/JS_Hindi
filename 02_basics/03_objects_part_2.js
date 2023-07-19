b12e831f5a0f48a042e638251256feba21eab2f9
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-19 14:23:22 +0000

    Objects Part 2

--- 02_basics/03_objects_part_2.js
+++ 02_basics/03_objects_part_2.js
@@ -1,6 +1,5 @@
 // const YtUser = new Object() //Declaring singleton object
 
-
 const YtUser = {} //Declaring non-singleton object
 YtUser.id = "0333123"
 YtUser.name = "Gagan"
@@ -19,9 +18,15 @@ const user1 = {
         password:"123321"
     }
 }
-console.log(YtUser)
-console.log(user1)
-console.log(user1.userProfile.password)
+console.log(YtUser)//{ id: '0333123', name: 'Gagan', isLoggedIn: false }
+console.log(user1)//output:
+//{
+//     email: 'admin@gmail.com',
+//     fullname: { name: 'Gagan', sirName: 'Gowda' },
+//     userProfile: { username: 'skyg', password: '123321' }
+//   }
+
+console.log(user1.userProfile.password)//123321
 
 
 //combining objects like arrays
@@ -32,6 +37,6 @@ const obj2 = {3:"c",4:"d"}
 // const obj3 = Object.assign({}, obj1,obj2);  //The Object.assign() static method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object.
 // console.log(obj3)
 const obj3 = {...obj1,...obj2}  //spread
-console.log(obj3)
+console.log(obj3)//{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 // console.log(obj3===obj1);
 
