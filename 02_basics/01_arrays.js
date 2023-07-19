c2e23d3a7278280a36102a72bff98c37dbda955d
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-19 14:04:18 +0000

    Arrays and Methods

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -14,19 +14,19 @@ myArr.unshift(10);   //unshift method adds element at index of [0]th position
 myArr.shift();  //shift removes first element
 */
 /*
-const newArr = myArr.join()//this method converts object into string means type of myArr is object and tyoe fo newArr is string.
+const newArr = myArr.join()//this method converts object into string means type of myArr is object and type of newArr is string.
 console.log(myArr);
 console.log(typeof(newArr)); //string
 */
 
 //Slice and Splice
 
-console.log("A ", myArr);//prints original array
+console.log("A", myArr);//prints original array
 
 const myArr1 = myArr.slice(1,3)//prints section of array and returns the copy of an array
 console.log(myArr1)
 console.log("B", myArr)
 
-const myArr2 = myArr.splice(1,3) //manipulates the original array and changes the elements of an oriiginal array
+const myArr2 = myArr.splice(1,3) //manipulates the original array and changes the elements of an original array
 console.log("C", myArr)
 console.log(myArr)
