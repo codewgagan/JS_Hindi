c538595ff63b1197837aa5d2df2be743a17b108e
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-22 15:36:16 +0000

    for each loop

--- 05_Iterations/for_each_loop.js
+++ 05_Iterations/for_each_loop.js
@@ -9,9 +9,26 @@
 // console.log(values)
 // for each use karte vakt call back function ka naam nahi hota hai.
 
+//filter
+
+//scope means curley braces{} if you open scope you have to write or use return keyword to get the result.
+
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 const newNums=  myNums.filter( (num) => num > 2 )
-console.log(newNums);
+console.log(newNums);//using filter
+//filter b call back function leta hai and condition dena padta hai.
+
+const nNums = []
+myNums.forEach( (num) => {
+    if (num>2) {
+        nNums.push(num)
+    }
+} )
+console.log(nNums);//using for each
+
+
+
 
 const mySkills = [
     {
