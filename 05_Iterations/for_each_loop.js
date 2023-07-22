7828f926771141a653e693ea6c386db7217f4b00
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-22 14:50:22 +0000

    for each loop

--- 05_Iterations/for_each_loop.js
+++ 05_Iterations/for_each_loop.js
@@ -13,3 +13,21 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const newNums=  myNums.filter( (num) => num > 2 )
 console.log(newNums);
 
+const mySkills = [
+    {
+        langName: "JS",
+        Rating: "Good"
+    },
+    {
+        langName: "Python",
+        Rating: "Best"
+    },
+    {
+        langName: "HTML",
+        Rating: "Easy"
+    },
+]
+mySkills.forEach( (item) => {
+    console.log(item.langName,item.Rating);
+})
+
