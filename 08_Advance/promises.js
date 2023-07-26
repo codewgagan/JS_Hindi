14cbd43fa453643daa28516f0db8c18d905be652
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-26 19:00:06 +0000

    Promise

--- 08_Advance/promises.js
+++ 08_Advance/promises.js
@@ -66,3 +66,10 @@ async function consumePromiseFive(){
     }
 }
 consumePromiseFive()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data);
+}).catch((error)=>console.log(error))
