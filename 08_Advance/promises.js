fe9525f702e64e9049450c96ee0649c9efd139c8
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-07-27 14:28:42 +0000

    Promise in JS

--- 08_Advance/promises.js
+++ 08_Advance/promises.js
@@ -1,7 +1,7 @@
 //A Promise is an object representing the eventual completion or failure of an asynchronous operation. 
 const promiseOne  = new Promise(function(resolve, reject){
   //Do an async task
-  //DB calls cryptography, network
+  //DB calls, cryptography, network
   setTimeout(function(){
       console.log('Async task is completed');
       resolve()
