42ebe6c93f90b799c9f613f639472caf854f4d84
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-08-18 14:30:49 +0000

    Conversion Operation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -11,6 +11,8 @@ console.log(typeof boolean)//number
 console.log(typeof booleanValue);//boolean
 console.log(booleanValue)//true
 
+//we can convert one data type into another datatype like number to string by using String.
+//we can convert one data type into another datatype like sttring to number by using Number.
 //"33" -> 33
 //"33abc"-> NaN
 //true -> 1; false->0;
