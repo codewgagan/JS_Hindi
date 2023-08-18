97138f14a7a13eec01f3799678b5226515fad4a0
Gagan Gowda <122716450+codewgagan@users.noreply.github.com>
2023-08-18 14:30:20 +0000

    Conversion operation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -2,9 +2,16 @@ let score = "333abd"
 console.log(typeof score)//String
 
 let valueInNumber = Number (score);
+console.log( typeof valueInNumber)//number
 console.log(valueInNumber)//NaN
 
 let boolean = 1
 let booleanValue = Boolean (boolean)
 console.log(typeof boolean)//number
+console.log(typeof booleanValue);//boolean
 console.log(booleanValue)//true
+
+//"33" -> 33
+//"33abc"-> NaN
+//true -> 1; false->0;
+//1->true; 0->false;
